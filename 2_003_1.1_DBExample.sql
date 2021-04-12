99bccc8e38796800b10bb91ac8c1c776ac9c84e1
Tevet <3jgzP42h>
2021-04-12 13:58:39 +0300

    001SQL Create-Insert-Update-Delete (Renaming DB)

--- 2_003_1.1_DBExample.sql
+++ 2_003_1.1_DBExample.sql
@@ -1,11 +1,2 @@
-create table customers (id serial primary key, name text, phone bigint, premium boolean);
-
-insert into customers (name, phone, premium) values ('Vasiliy Konov', 5639874585, false);
-insert into customers (name, premium) values ('Denis Kac', true);
-select * from customers;
-
-update customers set phone = 9968557414 where id = 2;
-select * from customers;
-
-delete from customers where id = 2;
-select * from customers;
+create table comments(id serial primary key, Content text);
+select * from comments;
